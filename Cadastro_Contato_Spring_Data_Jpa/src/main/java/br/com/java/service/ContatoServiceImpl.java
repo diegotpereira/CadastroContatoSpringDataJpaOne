11f866f8ae888a9ee8a13c6f912d19567cd68952
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-12-11 10:08:31 -0300

    criação do arquivo classe contatoserviceimpl.java

---
+++ Cadastro_Contato_Spring_Data_Jpa/src/main/java/br/com/java/service/ContatoServiceImpl.java
@@ -0,0 +1,53 @@
+package br.com.java.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.java.entity.Contato;
+import br.com.java.repository.ContatoRepository;
+
+@Service
+public class ContatoServiceImpl implements ContatoService{
+	
+	@Autowired
+	private ContatoRepository repo;
+
+	@Override
+	public Iterable<Contato> buscarTodos() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	@Override
+	public List<Contato> buscar(String contato) {
+		// TODO Auto-generated method stub
+		return repo.buscarPorNome(contato);
+	}
+
+	@Override
+	public Contato findOne(Integer id) {
+		// TODO Auto-generated method stub
+		Optional<Contato> contato = repo.findById(id);
+		
+		Contato cont = new Contato();
+		return cont;
+	}
+
+	@Override
+	public void salvar(Contato contato) {
+		// TODO Auto-generated method stub
+		repo.save(contato);
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		repo.deleteById(id);
+		
+	}
+
+}
