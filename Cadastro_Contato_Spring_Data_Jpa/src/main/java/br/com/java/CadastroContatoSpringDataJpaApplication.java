8c2baeb3e7a1a596ab05e842d063e941cd864264
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-12-11 09:06:03 -0300

    criação dos arquivos de configuração do projeto

---
+++ Cadastro_Contato_Spring_Data_Jpa/src/main/java/br/com/java/CadastroContatoSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroContatoSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroContatoSpringDataJpaApplication.class, args);
+	}
+
+}
